1ae5078ecc1299ba56cd91f6d94db2314c1336bb
200696 <200696@e0601-27.JC-21.local>
2023-05-23 10:59:09 +0900

    初回コミット

---
+++ src/test/java/jp/te4a/spring/boot/myapp4/ApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.te4a.spring.boot.myapp4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
